1e72cf8e25ae09b0d92306531a9f66680a8bb8d0
Fortune00 <tasha0415@naver.com>
2022-02-24 04:42:30 +0900

    feat : server function for init

--- server/index.js
+++ server/index.js
@@ -29,3 +29,23 @@ export async function isResponseFast(url, hr, sp02, time) {
     data: {method: 'isResponseFast', params: [hr, sp02, time]},
   });
 }
+
+export async function isDrowsyToInit(url, hr, spo2) {
+  //return true/false
+  return await axios({
+    url,
+    method: 'post',
+    header: {'Content-Type': 'application/json'},
+    data: {bpm: hr, spo2: spo2},
+  });
+}
+
+export async function isResponseFastToInit(url, time) {
+  //return string=filename
+  return await axios({
+    url,
+    method: 'post',
+    header: {'Content-Type': 'application/json'},
+    data: {time: time},
+  });
+}
